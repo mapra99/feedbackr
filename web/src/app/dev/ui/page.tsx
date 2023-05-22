6b676828e67f635c34cf023deaed055cafa4f29e
Miguel Prada <mapra99@gmail.com>
2023-05-22 16:35:27 -0500

    wip

---
+++ web/src/app/dev/ui/page.tsx
@@ -0,0 +1,7 @@
+export default function Ui() {
+  return (
+    <h1>
+      DESIGN SYSTEM
+    </h1>
+  )
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'veronica': '#AD1FEA',
+      'savoy-blue': '#4661E6',
+      'delft-blue': '#373F68',
+      'white': '#FFFFFF',
+      'ghost-white': '#F2F4FF',
+      'ghost-white-light': '#F7F8FD',
+      'marian-blue': '#3A4374',
+      'glaucous': '#647196',
+      'atomic-tangerine': '#F49F85',
+      'maya-blue': '#62BCFA'
+    },
+    fontFamily: {
+      sans: ['Jost', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
