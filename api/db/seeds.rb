7c65df71353f1c4e208fde90e3b87f30bdd6efd1
Miguel Prada <mapra99@gmail.com>
2023-06-02 12:13:33 -0500

    chore(local setup): automate setup

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Doorkeeper::Application.find_or_create_by(
+  name: 'Feedbackr Web Dev',
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+  scopes: %w[read write],
+  uid: 'uid123',
+  secret: 'supersecret'
+)
