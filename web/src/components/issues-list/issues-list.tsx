504a67242e94559ad374ed964fb1ba798f9c1375
Miguel Prada <mapra99@gmail.com>
2023-06-03 00:48:09 -0500

    empty issues state

--- web/src/components/issues-list/issues-list.tsx
+++ web/src/components/issues-list/issues-list.tsx
@@ -1,11 +1,24 @@
-import IssuesControls from '../issues-controls/issues-controls'
+import IssuesControls from '@/components/issues-controls'
+import NoIssuesBanner from '@/components/no-issues-banner'
 import type { IssuesListProps } from './types'
 
 export default function IssuesList({ issues }: IssuesListProps) {
   return (
     <div>
       <IssuesControls />
-      <h1>ISSUES LIST</h1>
+      { issues.length > 0 ? (
+        <div className="mt-8">
+          { issues.map(issue => (
+            <div key={issue.uuid} className="mt-4">
+              { issue.title }
+            </div>
+          )) }
+        </div>
+      ) : (
+        <div className="my-8 mx-6 sm:my-6 sm:mx-0">
+          <NoIssuesBanner />
+        </div>
+      )}
     </div>
   )
 }
---
+++ web/src/components/no-issues-banner/no-issues-banner.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/button'
+import { DetectiveIcon } from "@/icons"
+
+export default function NoIssuesBanner() {
+  return (
+    <div className="bg-white rounded-lg py-20 sm:py-28 px-6 flex justify-center items-center flex-col">
+      <div className="h-28 w-28 sm:w-36 sm:h-36 mb-10 sm:mb-14">
+        <DetectiveIcon />
+      </div>
+
+      <h2 className="font-sans text-marian-blue text-xl sm:text-3xl mb-3 sm:mb-4">
+        There is no feedback yet.
+      </h2>
+      <p className="font-sans text-glaucous text-xs sm:text-base text-center font-normal mb-6 sm:mb-12 max-w-md">
+        Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.
+      </p>
+      <Button variant="primary" className="!px-4 sm:!px-6 sm:h-12">
+        + Add Feedback
+      </Button>
+    </div>
+  )
+}
