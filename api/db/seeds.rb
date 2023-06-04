9e93f4e55de70cbd4e61831c892121c8e806357b
Miguel Prada <mapra99@gmail.com>
2023-06-04 18:44:35 -0500

    linter

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -49,9 +49,11 @@ Issue.find_or_create_by(
   user:
 )
 
-Comment.find_or_create_by(parent: issue, user:, content: "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.")
-comment = Comment.find_or_create_by(user:, parent: issue, content: "Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.")
+# rubocop:disable Layout/LineLength
+Comment.find_or_create_by(parent: issue, user:, content: 'Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.')
+comment = Comment.find_or_create_by(user:, parent: issue, content: 'Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.')
 reply = comment.replies.find_or_create_by(user:, content: '@hummingbird1 While waiting for dark mode, there are browser extensions that will also do the job. Search for "dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.')
 reply.replies.find_or_create_by(user:, content: "@annev1990  Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness. I'd prefer not to use one of such extensions, however, for security and privacy reasons.")
 
 comment.replies.find_or_create_by(user:, content: '@hummingbird1 While waiting for dark mode, there are browser extensions that will also do the job. Search for "dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.')
+# rubocop:enable Layout/LineLength
