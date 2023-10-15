27372052459d5028a248023ab01262d3f6d60a49
Miguel Prada <mapra99@gmail.com>
2023-10-14 19:28:06 -0500

    roadmap header

--- web/src/components/roadmap-header/index.tsx
+++ web/src/components/roadmap-header/index.tsx
@@ -1,10 +1,21 @@
+import BackButton from '@/components/back-button'
+import { LinkButton } from '@/components/button'
 import type { RoadmapHeaderProps } from './types'
 
 export default function RoadmapHeader({ productSlug }: RoadmapHeaderProps) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-gray-900">Roadmap</h1>
-      <p className="mt-4 text-gray-600">Product: {productSlug}</p>
+    <div className="flex items-center justify-between gap-4 px-6 md:px-8 lg:px-10 py-7 bg-delft-blue text-white font-sans md:rounded-xl">
+      <div className="flex flex-col gap-1">
+        <BackButton lightText />
+        <h1 className="text-xl md:text-3xl font-bold text-gray-900">Roadmap</h1>
+      </div>
+
+      <LinkButton
+        variant="primary" className="!px-3 sm:!px-6 sm:h-12"
+        href={`/${productSlug}/issues/new`}
+      >
+        + Add Feedback
+      </LinkButton>
     </div>
   )
 }
